5f6cbd9f5529f873879c2ee3ecd55cfb487fc87a
Kafui Alordo <kalordo7@gmail.com>
2023-02-23 17:12:34 +0000

    update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  branches: "master",
-  repositoryUrl: "https://github.com/kafui4k/react-ci-cd-workflow-app",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/github",
-      {
-        assets: [
-          {
-            path: "build.zip",
-            label: "Build"
-          },
-          {
-            path: "coverage.zip",
-            label: "Coverage"
-          }
-        ]
-      }
+  release: {
+    branches: ["master", "next"],
+    repositoryUrl: "https://github.com/kafui4k/react-ci-cd-workflow-app",
+    plugins: [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      [
+        {
+          assets: [
+            {
+              path: "build.zip",
+              label: "Build"
+            },
+            {
+              path: "coverage.zip",
+              label: "Coverage"
+            }
+          ]
+        }
+      ]
     ]
-  ]
+  }
 };
